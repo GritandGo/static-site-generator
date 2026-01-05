6ebf3360a9fecb3299cbccbfd088fb825906ee5b
GritandGo <codemain0123@gmail.com>
2026-01-04 20:46:36 -0600

    Add template, content, and generate_page logic for static site Wire main script to build public site from markdown

--- src/gen_content.py
+++ src/gen_content.py
@@ -1,3 +1,28 @@
+from markdown_to_html import markdown_to_html_node
+import os
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path) as f:
+        content = f.read()
+    with open(template_path) as f:
+        template = f.read()
+
+
+    node = markdown_to_html_node(content)
+    html = node.to_html()
+    title = extract_title(content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+
+
+    dir_path = os.path.dirname(dest_path)
+    if dir_path != "":
+        os.makedirs(dir_path, exist_ok=True)
+    with open(dest_path, 'w') as f:
+        f.write(template)
+
 
 
 
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 from copy_static import copy_files_recursive
-
+from gen_content import generate_page
 
 def main():
     src = "./static"
@@ -12,5 +12,13 @@ def main():
     
     copy_files_recursive(src, dst)
 
+
+    generate_page(
+        "content/index.md",
+        "template.html",
+        "public/index.html"
+    )
+
+    
 if __name__ == "__main__":
     main()
